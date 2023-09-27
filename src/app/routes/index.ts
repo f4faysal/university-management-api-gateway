4e81f54387b8b8c26ccf1200e06597fc440a8aaf
f4faysal <f4faysals@gmail.com>
2023-09-27 18:50:16 +0600

    Student And ADMIN creat

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -13,71 +13,75 @@ import { studentSemesterPaymentRoutes } from '../modules/studentSemesterPayment/
 import { userRoutes } from '../modules/user/user.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
-
+import { authRoutes } from '../modules/auth/auth.rotes';
 
 const router = express.Router();
 
 const moduleRoutes = [
-    {
-        path: '/users',
-        routes: userRoutes
-    },
-    {
-        path: '/academic-faculties',
-        routes: academicFacultyRoutes
-    },
-    {
-        path: '/academic-departments',
-        routes: academicDepartmentRoutes
-    },
-    {
-        path: '/academic-semesters',
-        routes: AcademicSemesterRoutes
-    },
-    {
-        path: '/buildings',
-        routes: buildingRoutes
-    },
-    {
-        path: '/rooms',
-        routes: roomRoutes
-    },
-    {
-        path: '/courses',
-        routes: courseRoutes
-    },
-    {
-        path: '/semester-registrations',
-        routes: semesterRegistrationRoutes
-    },
-    {
-        path: '/offered-courses',
-        routes: offeredCourseRoutes
-    },
-    {
-        path: '/offered-course-sections',
-        routes: offeredCourseSectionRoutes
-    },
-    {
-        path: '/offered-course-class-schedules',
-        routes: offeredCourseClassScheduleRoutes
-    },
-    {
-        path: '/student-enrolled-courses',
-        routes: studentEnrolledCourseRoutes
-    },
-    {
-        path: '/student-enrolled-course-marks',
-        routes: studentEnrolledCourseMarkRoutes
-    },
-    {
-        path: '/student-semester-payments',
-        routes: studentSemesterPaymentRoutes
-    }
+  {
+    path: '/auth',
+    routes: authRoutes
+  },
+  {
+    path: '/users',
+    routes: userRoutes
+  },
+  {
+    path: '/academic-faculties',
+    routes: academicFacultyRoutes
+  },
+  {
+    path: '/academic-departments',
+    routes: academicDepartmentRoutes
+  },
+  {
+    path: '/academic-semesters',
+    routes: AcademicSemesterRoutes
+  },
+  {
+    path: '/buildings',
+    routes: buildingRoutes
+  },
+  {
+    path: '/rooms',
+    routes: roomRoutes
+  },
+  {
+    path: '/courses',
+    routes: courseRoutes
+  },
+  {
+    path: '/semester-registrations',
+    routes: semesterRegistrationRoutes
+  },
+  {
+    path: '/offered-courses',
+    routes: offeredCourseRoutes
+  },
+  {
+    path: '/offered-course-sections',
+    routes: offeredCourseSectionRoutes
+  },
+  {
+    path: '/offered-course-class-schedules',
+    routes: offeredCourseClassScheduleRoutes
+  },
+  {
+    path: '/student-enrolled-courses',
+    routes: studentEnrolledCourseRoutes
+  },
+  {
+    path: '/student-enrolled-course-marks',
+    routes: studentEnrolledCourseMarkRoutes
+  },
+  {
+    path: '/student-semester-payments',
+    routes: studentSemesterPaymentRoutes
+  }
 ];
 
 moduleRoutes.forEach((route) => {
-    router.use(route.path, route.routes);
+  router.use(route.path, route.routes);
 });
 
 export default router;
